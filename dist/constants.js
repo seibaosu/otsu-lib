5741a4a0d6740b98b3c2d130ee2ccc7736927dc1
Seiba <seibaosu@gmail.com>
2023-09-11 12:32:18 +0700

    Add : Base url

---
+++ dist/constants.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BaseURL = void 0;
+exports.BaseURL = 'https://osu.ppy.sh';
---
+++ typings/constants.d.ts
@@ -0,0 +1,1 @@
+export declare const BaseURL = "https://osu.ppy.sh";
