836ca30229f16e25326a1346760be1d2e66cb1b9
Seiba <seibaosu@gmail.com>
2023-09-13 17:17:10 +0700

    Tedting .

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,9 @@
+/* Account Stuff */
 export * from './users/user_id';
 export * from './users/user_data';
 export * from './users/user_best_plays';
 export * from './users/user_first_place';
 export * from './users/user_recent_plays';
 
+/* Beatmaps Stuff */
 export * from './beatmaps/beatmap_update';
