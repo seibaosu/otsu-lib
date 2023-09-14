9c18c817ea4449312328620a74896aba8e50e74f
Seiba <seibaosu@gmail.com>
2023-09-14 15:36:04 +0700

    Update : Edit parameters for some functions .

--- src/functions/users/user_best_plays.ts
+++ src/functions/users/user_best_plays.ts
@@ -1,40 +1,42 @@
 import fetch from 'node-fetch';
 import { BaseURL } from '../../constants';
 
-export const getBestPlays = async (userId: number, mode: number) => {
+import { getUserId } from './user_id';
+
+export const getBestPlays = async (username: string, mode: number) => {
 
     try {
 
         let strmode;
         strmode = mode === 0 ? 'osu' : mode === 1 ? 'taiko' : mode === 2 ? 'fruits' : mode === 3 ? 'mania' : mode;
 
-        const req = await fetch(`${BaseURL}/users/${userId}/scores/best?mode=${strmode}&limit=100`);
-    
-        if (req.status == 200) {
+        const userId = await getUserId(username);
+        if (userId.status == 404) {
 
+            return {
+                status: 400,
+                message: 'NO_DATA'
+            }
+
+        } else {
+
+            const req = await fetch(`${BaseURL}/users/${userId.id}/scores/best?mode=${strmode}&limit=100`);
             const res = await req.json();
-            if (res.length === 0) {
-                return {
-                    status: 400,
-                    message: 'NO_DATA'
-                }
-            } else {
 
-                let replayId = [];
-                for (let i = 0; i < res.length; i++) {
+            let replayId = [];
+            for (let i = 0; i < res.length; i++) {
 
-                    const repId = await res[i];
+                const repId = await res[i];
 
-                    replayId.push(repId);
-
-                }
-
-                return {
-                    status: 200,
-                    data: replayId
-                }
+                replayId.push(repId);
 
             }
+
+            return {
+                status: 200,
+                data: replayId
+            }
+
         }
 
     } catch (err) {
--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ export class Otsu {
     * @param {String} username - Osu account username
     * @param {number} mode - Osu mode [ 0: osu!  1:Taiko  2:Catch  3:Mania ]
     */
-    async user_best_plays(username: number, mode: number = 0) {
+    async user_best_plays(username: string, mode: number = 0) {
 
         return getBestPlays(username, mode);
 
