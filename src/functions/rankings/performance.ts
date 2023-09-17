42156162ae08aacc9261eb9e64187a0fda29fd97
Seiba <seibaosu@gmail.com>
2023-09-18 06:08:46 +0700

    Update : Add rankings functions .

--- src/functions/rankings/performance.ts
+++ src/functions/rankings/performance.ts
@@ -15,9 +15,48 @@ export const rankingsByPerformance = async (mode: number) => {
 
         const $ = load(res);
 
-        const coba = $('#scores > div > table > tbody > tr').length;
+        const length = $('#scores > div > table > tbody > tr').length;
 
-        return coba;
+        if(length == 0) {
+
+            return {
+                status: 404,
+                message: 'NO_DATA'
+            }
+        } else {
+
+            const result = [];
+            for(let i = 1; i <= length; i++) {
+
+                let rank = $('#scores > div > table > tbody > tr:nth-child('+i+') > td.ranking-page-table__column.ranking-page-table__column--rank').text().trim().replace(/#/g, '');
+                let user = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(2) > div > a.ranking-page-table__user-link-text.js-usercard').text().trim();
+                let country = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(2) > div > a:nth-child(1) > span').attr('title');
+                let accuracy = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(3)').text().trim();
+                let playcount = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(4)').text().trim().replace(/,/g, '');
+                let performance = $('#scores > div > table > tbody > tr:nth-child('+i+') > td.ranking-page-table__column.ranking-page-table__column--focused').text().trim().replace(/,/g, '');
+                let ss = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(6)').text().trim();
+                let s = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(7)').text().trim();
+                let a = $('#scores > div > table > tbody > tr:nth-child('+i+') > td:nth-child(8)').text().trim();
+            
+                result.push({
+                    rank: parseInt(rank),
+                    user: user,
+                    country: country,
+                    accuracy: accuracy,
+                    playcount: parseInt(playcount),
+                    performance: parseInt(performance),
+                    ss: parseInt(ss),
+                    s: parseInt(s),
+                    a: parseInt(a)
+                });
+
+            }
+
+            return {
+                status: 200,
+                data: result
+            }
+        }
         
     } catch (err) {
         return err;
